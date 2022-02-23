00edf250fdfbca17e1cee822e7c3e86c26056711
Shivani Aggarwal <shivanisa33@gmail.com>
2022-02-23 15:53:55 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from './pages/Dashboard/Dashboard';
+import HealthRecords from './pages/HealthRecords/HealthRecords';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <p>Dashboard</p>
-      </header>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="record" element={<HealthRecords />} />
+      </Routes>
     </div>
   );
 }
