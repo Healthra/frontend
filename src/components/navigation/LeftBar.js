ca108f1ec15e8649a9a334f62654b2ee169d2a47
hyeyeon <hyeyeon@v1040-wn-rt-b-38-242.campus-dynamic.uwaterloo.ca>
2022-03-09 13:14:08 -0500

    leftbar

--- src/components/navigation/LeftBar.js
+++ src/components/navigation/LeftBar.js
@@ -3,6 +3,21 @@ import { slide as Menu } from "react-burger-menu";
 import "./styles.css"
 
 export default props => {
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
   return (
     <Menu {...props}>
       <a href="/">
@@ -11,9 +26,17 @@ export default props => {
       <a className="menu-item" href="/">
         Home
       </a>
-      <a className="menu-item" href="/record">
+      {/* <a className="menu-item" href="/record">
         Health Records
+      </a> */}
+      <a class="dropdown-btn" href="/record">Health Records
+        <i class="fa fa-caret-down"></i>
       </a>
+      <div class="dropdown-container">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
       <a className="menu-item" href="/appointments">
         Appointments
       </a>
