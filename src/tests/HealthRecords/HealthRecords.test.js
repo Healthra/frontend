31869aa1c66181cc6391da20443d608349345c24
Shivani Aggarwal <shivanisa33@gmail.com>
2022-03-14 18:00:35 -0400

    test changes

--- src/tests/HealthRecords/HealthRecords.test.js
+++ src/tests/HealthRecords/HealthRecords.test.js
@@ -1,2 +1,7 @@
 import HealthRecords from '../../pages/HealthRecords/HealthRecords';
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
 
+test('Page changes on navigation button click', async () => {
+    // const component = render(<HealthRecords/>);
+});
