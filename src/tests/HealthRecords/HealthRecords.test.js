a233f19d30e3d15d0c73b1ee46a11bf320049f48
Shivani Aggarwal <shivanisa33@gmail.com>
2022-03-14 19:07:47 -0400

    tests

--- src/tests/HealthRecords/HealthRecords.test.js
+++ src/tests/HealthRecords/HealthRecords.test.js
@@ -1,6 +1,7 @@
 import HealthRecords from '../../pages/HealthRecords/HealthRecords';
 import React from 'react';
 import {cleanup, fireEvent, render} from '@testing-library/react';
+import 'jest-canvas-mock';
 
 test('Page changes on navigation button click', async () => {
     // const component = render(<HealthRecords/>);
