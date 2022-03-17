49da77663256b3b758c8f7cae0a8d9de71191db1
Shivani Aggarwal <shivanisa33@gmail.com>
2022-03-17 11:12:22 -0400

    test updates

--- src/tests/HealthRecords/HealthRecords.test.js
+++ src/tests/HealthRecords/HealthRecords.test.js
@@ -2,7 +2,6 @@ import HealthRecords from '../../pages/HealthRecords/HealthRecords';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 
-
 test('Page changes on navigation button click', async () => {
     delete window.location
     window.location = new URL('http://localhost3000/frontend/record/medications');
@@ -10,8 +9,18 @@ test('Page changes on navigation button click', async () => {
     const renderer = new ShallowRenderer();
     renderer.render(<HealthRecords />);
     const result = renderer.getRenderOutput();
-    console.log(result.props.children);
-    console.log(result);
 
-    expect(result.type).toBe('div');
+    // expect(result.type).toBe('div');
+    // expect(result.props.id).toBe('records');
+
+    const body = result.props.children[1];
+
+    // expect(body.type).toBe('div');
+    // expect(body.props.className).toBe('body');
+
+    const navigation = body.props.children[0];
+    const content = body.props.children[1];
+
+    console.log(navigation.props.children[0]);
+    // console.log(content);
 });
